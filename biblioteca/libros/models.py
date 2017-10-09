b653fd450f15fb6ca7de2eba618940a2e10ef99c
jose <jose>
2017-10-09 11:54:11 -0700

    Creado modelo libro

--- biblioteca/libros/models.py
+++ biblioteca/libros/models.py
@@ -4,3 +4,12 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+from django.utils import timezone
+
+class libro(models.Model):
+    titulo = models.CharField(max_length=100)
+    autor = models.CharField(max_length=50)
+    editorial = models.CharField(max_length=100)
+    ISBN = models.CharField(max_length=32)
+    precio = models.IntegerField()
+    creado = models.DateTimeField(default = timezone.now)
